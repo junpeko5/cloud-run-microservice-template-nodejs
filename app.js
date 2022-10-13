88b941ea30cdc8b98757a3b2059188f5e754b9bc
junpeko5 <junpeko5@gmail.com>
2022-10-13 21:01:57 +0900

    fix: びっくりマークを増やした

--- app.js
+++ app.js
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 const express = require('express');
-const {pinoHttp, logger} = require('./utils/logging');
+const { pinoHttp, logger } = require('./utils/logging');
 
 const app = express();
 
@@ -23,10 +23,10 @@ app.use(pinoHttp);
 // Example endpoint
 app.get('/', async (req, res) => {
   // Use basic logger without HTTP request info
-  logger.info({logField: 'custom-entry', arbitraryField: 'custom-entry'}); // Example of structured logging
+  logger.info({ logField: 'custom-entry', arbitraryField: 'custom-entry' }); // Example of structured logging
   // Use request-based logger with log correlation
   req.log.info('Child logger with trace Id.'); // https://cloud.google.com/run/docs/logging#correlate-logs
-  res.send('Hello World!');
+  res.send('Hello World!!!!');
 });
 
 module.exports = app;
